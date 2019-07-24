d99702231205f620ac6c7c3301e0e9086d0e5519
ZdenekM <imaterna@fit.vutbr.cz>
2019-07-24 15:52:01 +0200

    From mypy docs on --no-implicit-reexport (turned on by --strict): By default, imported values to a module are treated as exported and mypy allows other modules to import them. This flag changes the behavior to not re-export unless the item is imported using from-as.

--- arcor2/object_types/__init__.py
+++ arcor2/object_types/__init__.py
@@ -1,5 +1,5 @@
-from arcor2.object_types.generic import Generic
-from arcor2.object_types.robot import Robot
-from arcor2.object_types.workspace import Workspace
+from arcor2.object_types.generic import Generic as Generic
+from arcor2.object_types.robot import Robot as Robot
+from arcor2.object_types.workspace import Workspace as Workspace
 
 __all__ = ["Generic", "Robot", "Workspace"]
