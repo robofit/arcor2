295bfe7af8fbf541c285f5ced12ab188541c0a58
Michal Kapinus <michal.kapinus@gmail.com>
2019-08-09 12:26:12 +0200

    conflict resolved manualy

--- arcor2/object_types/upload.py
+++ arcor2/object_types/upload.py
@@ -29,19 +29,9 @@ def upload(module_cls: str) -> None:
 
     path = inspect.getfile(cls)
 
-<<<<<<< HEAD
     
-    try:
-        MONGO_ADDRESS = os.environ["ARCOR2_MONGO_ADDRESS"]
-        mongo = MongoClient(MONGO_ADDRESS.split(':')[0], int(MONGO_ADDRESS.split(':')[1]))
-    except (ValueError, IndexError) as e:
-        sys.exit("'ARCOR2_MONGO_ADDRESS' env. variable not well formated. Correct format is 'hostname:port'")
-    except KeyError:
-        sys.exit("'ARCOR2_MONGO_ADDRESS' env. variable not set.")
-    db = mongo.arcor2
-=======
     storage_client = PersistentStorageClient()
->>>>>>> 7dd0733cfb69bb40b3cd2814de41c881888aa0fb
+
 
     with open(path, "r") as source_file:
 
@@ -56,4 +46,6 @@ def upload(module_cls: str) -> None:
         obj_type = ObjectType(id=cls.__name__, source=source)
 
         print(f"Storing '{obj_type.id}'...")
+
         storage_client.update_object_type(obj_type)
+        
