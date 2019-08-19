b0cc5ca316de7522f50b3f9cf7237124a86ee905
ZdenekM <imaterna@fit.vutbr.cz>
2019-08-19 10:37:26 +0200

    Storage client: simplified code, proper type hints, handling of errors.

--- arcor2/persistent_storage_client.py
+++ arcor2/persistent_storage_client.py
@@ -2,77 +2,101 @@ import requests
 import os
 import json
 import asyncio
+from typing import Type, TypeVar
+
+from dataclasses_jsonschema import ValidationError, JsonSchemaMixin
 
 from arcor2.data.common import Project, Scene, ProjectSources, ObjectType, DataClassEncoder, IdDescList
 from arcor2.exceptions import Arcor2Exception
 
 URL = os.getenv("ARCOR2_PERSISTENT_STORAGE_URL", "http://127.0.0.1:5001")
 
-# TODO handle errors
-# TODO cache?
-# TODO poll changes?
+# TODO logger
+# TODO rename to PersistentStorage (and remove nodes/persistent_storage.py)
+# TODO thread to poll changes? how to "detect" changes?
+
+TIMEOUT = (0.1, 0.25)  # connect, read
 
 
 class PersistentStorageClientException(Arcor2Exception):
     pass
 
 
+T = TypeVar('T', bound=JsonSchemaMixin)
+
+
 class PersistentStorageClient:
 
-    def get_projects(self) -> IdDescList:
+    def _post(self, url: str, data: JsonSchemaMixin):
 
-        resp = requests.get(f"{URL}/projects")
-        return IdDescList.from_dict(json.loads(resp.text))
+        try:
+            resp = requests.post(url, json=json.dumps(data, cls=DataClassEncoder), timeout=TIMEOUT)
+            resp.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            raise PersistentStorageClientException("Catastrophic error.")
+
+    def _get(self, url: str, data_cls: Type[T]) -> T:
+
+        try:
+            resp = requests.get(url, timeout=TIMEOUT)
+            resp.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            raise PersistentStorageClientException("Catastrophic error.")
+
+        try:
+            data = json.loads(resp.text)
+        except (json.JSONDecodeError, TypeError) as e:
+            print(e)
+            raise PersistentStorageClientException("Invalid JSON.")
+
+        try:
+            return data_cls.from_dict(data)
+        except ValidationError as e:
+            print(e)
+            raise PersistentStorageClientException("Invalid data.")
+
+    def get_projects(self) -> IdDescList:
+        return self._get(f"{URL}/projects", IdDescList)
 
     def get_scenes(self) -> IdDescList:
-
-        resp = requests.get(f"{URL}/scenes")
-        return IdDescList.from_dict(json.loads(resp.text))
+        return self._get(f"{URL}/scenes", IdDescList)
 
     def get_project(self, project_id: str) -> Project:
-
-        resp = requests.get(f"{URL}/project/{project_id}")
-        return Project.from_dict(json.loads(resp.text))
+        return self._get(f"{URL}/project/{project_id}", Project)
 
     def get_project_sources(self, project_id: str) -> ProjectSources:
-
-        resp = requests.get(f"{URL}/project/{project_id}/sources")
-        return ProjectSources.from_dict(json.loads(resp.text))
+        return self._get(f"{URL}/project/{project_id}/sources", ProjectSources)
 
     def get_scene(self, scene_id: str) -> Scene:
-
-        resp = requests.get(f"{URL}/scene/{scene_id}")
-        return Scene.from_dict(json.loads(resp.text))
+        return self._get(f"{URL}/scene/{scene_id}", Scene)
 
     def get_object_type(self, object_type_id: str) -> ObjectType:
-
-        resp = requests.get(f"{URL}/object_type/{object_type_id}")
-        return ObjectType.from_dict(json.loads(resp.text))
+        return self._get(f"{URL}/object_type/{object_type_id}", ObjectType)
 
     def get_object_type_ids(self) -> IdDescList:
-        resp = requests.get(f"{URL}/object_types")
-        return IdDescList.from_dict(json.loads(resp.text))
+        return self._get(f"{URL}/object_types", IdDescList)
 
-    def update_project(self, project: Project):
+    def update_project(self, project: Project) -> None:
 
         assert project.id
-        requests.post(f"{URL}/project", json=json.dumps(project, cls=DataClassEncoder))
+        self._post(f"{URL}/project", project)
 
-    def update_scene(self, scene: Scene):
+    def update_scene(self, scene: Scene) -> None:
 
         assert scene.id
-        requests.post(f"{URL}/scene", json=json.dumps(scene, cls=DataClassEncoder))
+        self._post(f"{URL}/scene", scene)
 
-    def update_project_sources(self, project_sources: ProjectSources):
+    def update_project_sources(self, project_sources: ProjectSources) -> None:
 
         assert project_sources.id
-        requests.post(f"{URL}/project/sources",
-                      json=json.dumps(project_sources, cls=DataClassEncoder))
+        self._post(f"{URL}/project/sources", project_sources)
 
-    def update_object_type(self, object_type: ObjectType):
+    def update_object_type(self, object_type: ObjectType) -> None:
 
         assert object_type.id
-        requests.post(f"{URL}/object_type", json=json.dumps(object_type, cls=DataClassEncoder))
+        self._post(f"{URL}/object_type", object_type)
 
 
 loop = asyncio.get_event_loop()
@@ -85,18 +109,13 @@ class AioPersistentStorageClient:
         self._cl = PersistentStorageClient()
 
     async def get_projects(self) -> IdDescList:
-
-        res: IdDescList = await loop.run_in_executor(None, self._cl.get_projects)
-        return res
+        return await loop.run_in_executor(None, self._cl.get_projects)
 
     async def get_scenes(self) -> IdDescList:
-        res: IdDescList = await loop.run_in_executor(None, self._cl.get_scenes)
-        return res
+        return await loop.run_in_executor(None, self._cl.get_scenes)
 
     async def get_project(self, project_id: str) -> Project:
-
-        res: Project = await loop.run_in_executor(None, self._cl.get_project, project_id)
-        return res
+        return await loop.run_in_executor(None, self._cl.get_project, project_id)
 
     async def get_project_sources(self, project_id: str) -> ProjectSources:
         return await loop.run_in_executor(None, self._cl.get_project_sources, project_id)
