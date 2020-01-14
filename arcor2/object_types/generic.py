1b5b714c8c3f0b93d19c20a486c5d55e0d3e8cbe
ZdenekM <imaterna@fit.vutbr.cz>
2020-01-14 12:10:16 +0100

    'rate' action, default value and range for 'sleep'.

--- arcor2/object_types/generic.py
+++ arcor2/object_types/generic.py
@@ -22,6 +22,7 @@ class Generic(metaclass=abc.ABCMeta):
         self.pose = pose
         self.collision_model = collision_model
         self.action_points: Dict[str, ActionPoint] = {}
+        self._rate_start_time: Optional[float] = None
 
     @classmethod
     def description(cls) -> str:  # TODO mixin with common stuff for objects/services?
@@ -38,10 +39,34 @@ class Generic(metaclass=abc.ABCMeta):
         pass
 
     @action
-    def sleep(self, seconds: float) -> None:
+    def sleep(self, seconds: float = 1.0) -> None:
+
+        assert 0.0 <= seconds <= 10.0e6
+
         time.sleep(seconds)
 
-    # TODO more "general" actions like store_value, get_value, etc.
+    @action
+    def rate(self, period: float = 1.0) -> None:
+        """
+        Can be used to maintain desired rate of the loop. Should be the first action.
+        :param period: Desired period in seconds.
+        :return:
+        """
 
+        assert 0.0 <= period <= 10.0e6
+
+        now = time.monotonic()
+
+        if self._rate_start_time is None:
+            self._rate_start_time = now
+            return
+
+        dif = self._rate_start_time + period - now
+
+        if dif > 0:
+            time.sleep(dif)
+
+    # TODO more "general" actions like store_value, get_value, etc.
     nop.__action__ = ActionMetadata()  # type: ignore
     sleep.__action__ = ActionMetadata(blocking=True)  # type: ignore
+    rate.__action__ = ActionMetadata(blocking=True)  # type: ignore
