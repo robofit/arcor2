85a813ab01af538e9714771af93302aa68a1dd81
ZdenekM <imaterna@fit.vutbr.cz>
2020-03-06 14:14:28 +0100

    Boolean parameter plugin.

---
+++ arcor2/parameter_plugins/boolean.py
@@ -0,0 +1,17 @@
+from arcor2.data.common import Project, Scene
+from arcor2.parameter_plugins.base import ParameterPlugin, TypesDict
+
+
+class BooleanPlugin(ParameterPlugin):
+
+    @classmethod
+    def type(cls):
+        return bool
+
+    @classmethod
+    def type_name(cls) -> str:
+        return "boolean"
+
+    @classmethod
+    def value(cls, type_defs: TypesDict, scene: Scene, project: Project, action_id: str, parameter_id: str) -> str:
+        return cls.type()(super(BooleanPlugin, cls).value(type_defs, scene, project, action_id, parameter_id))
