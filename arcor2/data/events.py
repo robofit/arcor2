5f8158b23f50ca487b1cd7082c2b2fb18fdbdf7c
ZdenekM <imaterna@fit.vutbr.cz>
2020-04-10 08:45:11 +0200

    Server: notification when action is being executed (during project edit).

--- arcor2/data/events.py
+++ arcor2/data/events.py
@@ -163,6 +163,22 @@ class ActionStateEvent(Event):
 
 
 @dataclass
+class ActionExecutionData(JsonSchemaMixin):
+
+    action_id: str = ""
+
+
+@dataclass
+class ActionExecutionEvent(Event):
+    """
+    Used to notify beginning of the action execution triggered while editing a project.
+    """
+
+    data: ActionExecutionData = field(default_factory=ActionExecutionData)
+    event: str = field(default=wo_suffix(__qualname__), init=False)  # type: ignore  # noqa: F821
+
+
+@dataclass
 class ActionResult(JsonSchemaMixin):
 
     action_id: str = ""
@@ -172,6 +188,9 @@ class ActionResult(JsonSchemaMixin):
 
 @dataclass
 class ActionResultEvent(Event):
+    """
+    Used to notify result of the action execution triggered while editing a project.
+    """
 
     data: ActionResult = field(default_factory=ActionResult)
     event: str = field(default=wo_suffix(__qualname__), init=False)  # type: ignore  # noqa: F821
