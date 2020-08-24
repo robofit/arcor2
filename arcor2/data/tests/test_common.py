a6f05ad76e20da993fe13ac0fe8abe801c90136f
ZdenekM <imaterna@fit.vutbr.cz>
2020-08-24 12:24:44 +0200

    Handle invalid orientations.

---
+++ arcor2/data/tests/test_common.py
@@ -0,0 +1,24 @@
+import pytest  # type: ignore
+
+from arcor2.data.common import Orientation
+from arcor2.exceptions import Arcor2Exception
+
+
+def test_valid_orientation() -> None:
+
+    o1 = Orientation(0, 0, 0, 1)
+    o2 = Orientation()
+    o2.set_from_quaternion(o1.as_quaternion())
+    assert o1 == o2
+
+
+def test_invalid_orientation() -> None:
+
+    with pytest.raises(Arcor2Exception):
+        Orientation(0, 0, 0, 0)
+
+    o = Orientation()
+    o.w = 0
+
+    with pytest.raises(Arcor2Exception):
+        o.as_quaternion()
