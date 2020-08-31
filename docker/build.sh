96bed4590d8c01debe482d727c4c60f183a2bac8
Michal Kapinus <michal.kapinus@gmail.com>
2020-08-31 14:30:49 +0200

    updated build.sh script

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,7 @@
-#!/bin/bash 
-docker build -f Dockerfile-arserver -t arcor2/arcor2_arserver:`cat ../arcor2/VERSION`  ../ --build-arg version=`cat ../arcor2/VERSION`
-docker build -f Dockerfile-build -t arcor2/arcor2_build:`cat ../arcor2/VERSION` --build-arg version=`cat ../arcor2/VERSION` ../
-docker build -f Dockerfile-execution -t arcor2/arcor2_execution:`cat ../arcor2/VERSION` --build-arg version=`cat ../arcor2/VERSION` ../   
+#!/bin/bash
+VERSION=`python3 ../setup.py --version`
+#VERSION='calibration.1'
+docker build -f Dockerfile-arserver -t arcor2/arcor2_arserver:$VERSION  ../ --build-arg version=$VERSION
+docker build -f Dockerfile-build -t arcor2/arcor2_build:$VERSION --build-arg version=$VERSION ../
+docker build -f Dockerfile-execution -t arcor2/arcor2_execution:$VERSION --build-arg version=$VERSION ../
+docker build -f Dockerfile-mocks -t arcor2/arcor2_mocks:$VERSION --build-arg version=$VERSION ../
