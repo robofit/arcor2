6d56522e07f8eaedc94d1b71e66f22ef63802a72
ZdenekM <imaterna@fit.vutbr.cz>
2020-09-11 15:13:16 +0200

    Fixed test - object was missing in scene.

--- src/python/arcor2_build/source/tests/test_logic.py
+++ src/python/arcor2_build/source/tests/test_logic.py
@@ -1,17 +1,18 @@
 from arcor2.cached import CachedProject, CachedScene
-from arcor2.data.common import Action, ActionPoint, Flow, LogicItem, Position, Project, Scene
+from arcor2.data.common import Action, ActionPoint, Flow, LogicItem, Position, Project, Scene, SceneObject
 from arcor2_build.source.logic import program_src
 
 
 def test_program_src() -> None:
 
     scene = Scene("s1", "s1")
+    scene.objects.append(SceneObject("TestId", "test_name", "Test"))
     project = Project("p1", "p1", "s1")
     ap1 = ActionPoint("ap1", "ap1", Position())
     project.action_points.append(ap1)
 
-    ap1.actions.append(Action("ac1", "ac1", "Test/test", flows=[Flow()]))
-    ap1.actions.append(Action("ac2", "ac2", "Test/test", flows=[Flow()]))
+    ap1.actions.append(Action("ac1", "ac1", "TestId/test", flows=[Flow()]))
+    ap1.actions.append(Action("ac2", "ac2", "TestId/test", flows=[Flow()]))
 
     project.logic.append(LogicItem("l1", LogicItem.START, "ac1"))
     project.logic.append(LogicItem("l2", "ac1", "ac2"))
@@ -19,5 +20,5 @@ def test_program_src() -> None:
 
     src = program_src(CachedProject(project), CachedScene(scene), set())
 
-    assert "test.test(res.ac1)" in src
-    assert "test.test(res.ac2)" in src
+    assert "test_name.test(res.ac1)" in src
+    assert "test_name.test(res.ac2)" in src
