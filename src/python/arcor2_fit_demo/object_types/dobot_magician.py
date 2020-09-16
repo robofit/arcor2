906fee46d448f05dfdc4e251108856d13e09a889
ZdenekM <imaterna@fit.vutbr.cz>
2020-09-16 15:49:45 +0200

    Dobot M1: suck/release.

--- src/python/arcor2_fit_demo/object_types/dobot_magician.py
+++ src/python/arcor2_fit_demo/object_types/dobot_magician.py
@@ -1,8 +1,10 @@
 from typing import List
 
-from arcor2.data.common import Joint, StrEnum
+from pydobot import dobot  # type: ignore
 
-from .abstract_dobot import AbstractDobot
+from arcor2.data.common import ActionMetadata, Joint, StrEnum
+
+from .abstract_dobot import AbstractDobot, DobotException
 
 
 class Joints(StrEnum):
@@ -38,3 +40,26 @@ class DobotMagician(AbstractDobot):
             Joint(Joints.J4, -joints.j3),
             Joint(Joints.J5, joints.j4),
         ]
+
+    def suck(self) -> None:
+
+        if self.settings.simulator:
+            return
+
+        try:
+            self._dobot.wait_for_cmd(self._dobot.suck(True))
+        except dobot.DobotException as e:
+            raise DobotException("Suck failed.") from e
+
+    def release(self) -> None:
+
+        if self.settings.simulator:
+            return
+
+        try:
+            self._dobot.wait_for_cmd(self._dobot.suck(False))
+        except dobot.DobotException as e:
+            raise DobotException("Release failed.") from e
+
+    suck.__action__ = ActionMetadata(blocking=True)  # type: ignore
+    release.__action__ = ActionMetadata(blocking=True)  # type: ignore
