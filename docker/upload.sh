cb6837f53d1c19d67321d08a2c05a5aac2b1790b
Michal Kapinus <michal.kapinus@gmail.com>
2020-09-18 10:26:47 +0200

    versions for build and upload scripts taken from respective VERSION files

--- docker/upload.sh
+++ docker/upload.sh
@@ -2,9 +2,9 @@
 #VERSION=`python3 ../setup.py --version`
 VERSION='0.8.0rc3'
 #docker image push arcor2/arcor2_base:$VERSION
-docker image push arcor2/arcor2_arserver:$VERSION
-docker image push arcor2/arcor2_execution:$VERSION
-docker image push arcor2/arcor2_execution_proxy:$VERSION
-docker image push arcor2/arcor2_build:$VERSION
-docker image push arcor2/arcor2_mocks:$VERSION
-docker image push arcor2/arcor2_devel:$VERSION
+docker image push arcor2/arcor2_arserver:`cat ../src/python/arcor2_arserver/VERSION`
+docker image push arcor2/arcor2_execution:`cat ../src/python/arcor2_execution/VERSION`
+docker image push arcor2/arcor2_execution_proxy:`cat ../src/python/arcor2_execution_proxy/VERSION`
+docker image push arcor2/arcor2_build:`cat ../src/python/arcor2_build/VERSION`
+docker image push arcor2/arcor2_mocks:`cat ../src/python/arcor2_mocks/VERSION`
+#docker image push arcor2/arcor2_devel:`cat ../src/python/arcor2_arserver/VERSION`
