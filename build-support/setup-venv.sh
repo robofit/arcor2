c5b21466d4aaae9d504525b93768650bd51c4449
ZdenekM <imaterna@fit.vutbr.cz>
2020-10-02 09:13:03 +0200

    build: Some warnings resolved, update of dependencies, venv is now explicitly based on Python 3.8.

--- build-support/setup-venv.sh
+++ build-support/setup-venv.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-PYTHON_BIN=python3  # TODO use python3.8
+# on Ubuntu 18.04 it is necessary to install Python 3.8 first
+# see https://vsupalov.com/developing-with-python3-8-on-ubuntu-18-04/
+# also don't forget to run sudo apt install python3.8-venv python3.8-dev (otherwise ensurepip module won't be available)
+
+PYTHON_BIN=python3.8
 VIRTUALENV=build-support/.venv
 PIP="${VIRTUALENV}/bin/pip"
 
