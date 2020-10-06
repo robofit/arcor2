9b51cbbd68d5f1a88435e17874fc22b75312d028
ZdenekM <imaterna@fit.vutbr.cz>
2020-10-06 12:17:36 +0200

    ci: Shell Linter.

--- build-support/nuke-cache.sh
+++ build-support/nuke-cache.sh
@@ -3,10 +3,10 @@
 function nuke_if_too_big() {
   path=$1
   limit_mb=$2
-  size_mb=$(du -m -d0 ${path} | cut -f 1)
-  if (( ${size_mb} > ${limit_mb} )); then
+  size_mb=$(du -m -d0 "${path}" | cut -f 1)
+  if (( size_mb > limit_mb )); then
     echo "${path} is too large (${size_mb}mb), nuking it."
-    rm -rf ${path}
+    rm -rf "${path}"
   fi
 }
 
--- docker/start-execution-proxy.sh
+++ docker/start-execution-proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 
-cd
+cd || exit
 ./execution_rest_proxy.pex
