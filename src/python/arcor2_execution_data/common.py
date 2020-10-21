17c10703e3d26cba7e3029abfcd00221b3753d3b
ZdenekM <imaterna@fit.vutbr.cz>
2020-10-21 09:15:45 +0200

    feat: implementation of Execution API 0.4.0
    BREAKING CHANGE: PackageSummary model (used by Execution/ARServer) changed

--- src/python/arcor2_execution_data/common.py
+++ src/python/arcor2_execution_data/common.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from datetime import datetime
+from typing import Optional
 
 from dataclasses_jsonschema import JsonSchemaMixin
 
@@ -7,11 +8,17 @@ from arcor2.data.execution import PackageMeta
 
 
 @dataclass
+class ProjectMeta(JsonSchemaMixin):
+    """Provides data about the project embedded in the execution package."""
+
+    id: str
+    name: str
+    modified: datetime = field(metadata=dict(description="Last modification."))
+
+
+@dataclass
 class PackageSummary(JsonSchemaMixin):
 
     id: str
-    project_id: str
-    modified: datetime = field(
-        metadata=dict(description="Last modification of the project embedded in the execution package.")
-    )
     package_meta: PackageMeta = field(metadata=dict(description="Content of 'package.json'."))
+    project_meta: Optional[ProjectMeta] = None
