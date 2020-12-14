68038340be8bff9b4264f9608a40172eda82932e
Michal Kapinus <michal.kapinus@gmail.com>
2020-12-14 17:22:29 +0100

    feat: dockerfiles (#393)
    * feat: dockerfiles
    
    * added new dependencies to arserver and execution
    * added kinect azure mockup dockerfile
    * added kinect azure service to docker-compose
    * dockerfiles added to .dockerignore

--- docker/build.sh
+++ docker/build.sh
@@ -20,6 +20,10 @@ build_execution_image () {
 	docker build -f Dockerfile-execution -t arcor2/arcor2_execution:"$(cat ../src/python/arcor2_execution/VERSION)" --build-arg version="$VERSION" ../
 }
 
+build_kinect_azure () {
+	docker build -f Dockerfile-kinect-azure -t arcor2/arcor2_kinect_azure:"$(cat ../src/python/arcor2_kinect_azure/VERSION)" --build-arg version="$VERSION" ../
+}
+
 build_execution_proxy_image () {
 	docker build -f Dockerfile-execution-proxy -t arcor2/arcor2_execution_proxy:"$(cat ../src/python/arcor2_execution_rest_proxy/VERSION)" --build-arg version="$VERSION" ../
 }
@@ -46,6 +50,7 @@ build_all_images() {
 	build_build_image
 	build_execution_image
 	build_execution_proxy_image
+        build_kinect_azure
 	build_mocks_image
 	build_devel_image
 	build_upload_fit_demo_image
@@ -54,7 +59,7 @@ build_all_images() {
 
 if [ $# -eq 0 ]; then
     echo "Usage:"
-    echo "sudo sh build.sh VERSION [arserver] [build] [execution] [execution-proxy] [mocks] [devel] [upload-fit-demo] [upload-kinali]"
+    echo "sudo sh build.sh VERSION [arserver] [build] [execution] [execution-proxy] [kinect-azure] [mocks] [devel] [upload-fit-demo] [upload-kinali]"
     echo "sudo sh build.sh VERSION all"
     echo "$VERSION specifies version of base image. Optional parametes specifies which images should be build using version from their VERSION file."
     echo "If no optional parameter is specified, base image is build with version $VERSION."
@@ -80,29 +85,30 @@ fi
 
 for var in "$@"
 do
+    if [ "$var" = "$1" ]; then
+        continue
+    fi
     if [ "$var" = "arserver" ]; then
     	build_arserver_image
-	fi
-	if [ "$var" = "build" ]; then
+	elif [ "$var" = "build" ]; then
     	build_build_image
-	fi
-	if [ "$var" = "execution" ]; then
+	elif [ "$var" = "execution" ]; then
     	build_execution_image
-	fi
-	if [ "$var" = "execution_proxy" ]; then
+	elif [ "$var" = "execution-proxy" ]; then
     	build_execution_proxy_image
-	fi
-	if [ "$var" = "mocks" ]; then
+	elif [ "$var" = "kinect-azure" ]; then
+        build_kinect_azure
+        elif [ "$var" = "mocks" ]; then
     	build_mocks_image
-	fi
-	if [ "$var" = "devel" ]; then
+	elif [ "$var" = "devel" ]; then
     	build_devel_image
-	fi
-	if [ "$var" = "upload-fit-demo" ]; then
+	elif [ "$var" = "upload-fit-demo" ]; then
     	build_upload_fit_demo_image
-	fi
-	if [ "$var" = "upload-kinali" ]; then
+	elif [ "$var" = "upload-kinali" ]; then
     	build_upload_kinali_image
+	else
+	    echo "Unknown image $var"
+            exit 0
 	fi
 done
 
