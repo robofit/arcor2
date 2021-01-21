61d4fa7ae5e73a41b3ab469ff8576326312ef9bb
Michal Kapinus <michal.kapinus@gmail.com>
2021-01-21 18:47:49 +0100

    fix: another fix of upload

--- docker/upload.sh
+++ docker/upload.sh
@@ -6,7 +6,7 @@ docker image push arcor2/arcor2_arserver:"$(cat ../src/python/arcor2_arserver/VE
 docker image push arcor2/arcor2_execution:"$(cat ../src/python/arcor2_execution/VERSION)"
 docker image push arcor2/arcor2_execution_proxy:"$(cat ../src/python/arcor2_execution_rest_proxy/VERSION)"
 docker image push arcor2/arcor2_build:"$(cat ../src/python/arcor2_build/VERSION)"
-docker image push arcor2/arcor2_devel:$VERSION
+#docker image push arcor2/arcor2_devel:$VERSION
 docker image push arcor2/arcor2_upload_fit_demo:"$(cat ../src/python/arcor2_fit_demo/VERSION)"
 docker image push arcor2/arcor2_upload_kinali:"$(cat ../src/python/arcor2_kinali/VERSION)"
 docker image push arcor2/arcor2_mocks:"$(cat ../src/python/arcor2_mocks/VERSION)"
