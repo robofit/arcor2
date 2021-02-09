408f8ce0287cda5f5b8e038d167888b67e3252f0
ZdenekM <imaterna@fit.vutbr.cz>
2021-02-09 13:51:46 +0100

    fix(arcor2): action decorator fixed

--- src/python/arcor2/tests/test_action.py
+++ src/python/arcor2/tests/test_action.py
@@ -1,9 +1,12 @@
 import io
 from typing import Optional
 
+import pytest
+
 from arcor2.action import patch_object_actions
 from arcor2.data.common import ActionMetadata
 from arcor2.data.events import ActionStateAfter, ActionStateBefore
+from arcor2.exceptions import Arcor2Exception
 from arcor2.object_types.abstract import Generic
 
 
@@ -40,3 +43,35 @@ def test_patch_object_actions(monkeypatch, capsys) -> None:
 
     assert before_evt.data.action_id == "id"
     assert after_evt.data.action_id == "id"
+
+    with pytest.raises(Arcor2Exception):
+        my_obj.action()
+
+    with pytest.raises(Arcor2Exception):
+        my_obj.action(an="unknown_action_name")
+
+
+def test_patch_object_actions_without_mapping(monkeypatch, capsys) -> None:
+    class MyObject(Generic):
+        def action(self, *, an: Optional[str] = None) -> None:
+            pass
+
+        action.__action__ = ActionMetadata()  # type: ignore
+
+    # @action tries to read from stdin
+    sio = io.StringIO()
+    sio.fileno = lambda: 0  # type: ignore  # fake whatever fileno
+    monkeypatch.setattr("sys.stdin", sio)
+
+    obj_id = "123"
+
+    my_obj = MyObject(obj_id, "")
+    patch_object_actions(MyObject)  # no mapping given
+
+    my_obj.action()  # this should be ok
+    out_before, _ = capsys.readouterr()
+    assert not out_before
+
+    my_obj.action(an="whatever")  # why would anyone do this... but should be also ok
+    out_before, _ = capsys.readouterr()
+    assert not out_before
