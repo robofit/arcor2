00bcee5cf7f0c43586807bdf526dc852c864e5b0
ZdenekM <imaterna@fit.vutbr.cz>
2021-03-22 15:39:43 +0100

    feat: use monkey patching instead of custom released version
    - This is to deal with a bug in dataclasses_jsonschema.

--- src/python/arcor2/data/__init__.py
+++ src/python/arcor2/data/__init__.py
@@ -1,14 +1,34 @@
 import importlib
 import inspect
 import pkgutil
+from typing import Dict
 
 from dataclasses_jsonschema import JsonSchemaMixin, ValidationError
+from dataclasses_jsonschema.apispec import DataclassesPlugin, _schema_reference
 
 from arcor2.exceptions import Arcor2Exception
 
 
 class DataException(Arcor2Exception):
     pass
+
+
+def resolve_schema_refs(self, data: Dict) -> None:
+
+    if "schema" in data:
+
+        if "$ref" in data["schema"]:
+            data["schema"]["$ref"] = _schema_reference(data["schema"]["$ref"], self._schema_type)
+        elif "items" in data["schema"] and "$ref" in data["schema"]["items"]:
+            data["schema"]["items"]["$ref"] = _schema_reference(data["schema"]["items"]["$ref"], self._schema_type)
+    else:
+        for key in data:
+            if isinstance(data[key], dict):
+                self.resolve_schema_refs(data[key])
+
+
+# monkey patch to solve https://github.com/s-knibbs/dataclasses-jsonschema/issues/126
+DataclassesPlugin.resolve_schema_refs = resolve_schema_refs  # type: ignore
 
 
 def compile_json_schemas() -> None:
