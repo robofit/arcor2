6e9865495d9ef145e9bec33516359830ba4c201d
ZdenekM <imaterna@fit.vutbr.cz>
2021-04-30 13:22:26 +0200

    feat(arcor2/ws_server): parallel processing of RPC requests
    - A task is created for each request.

--- src/python/arcor2/ws_server.py
+++ src/python/arcor2/ws_server.py
@@ -46,6 +46,116 @@ async def server(
     event_dict: Optional[EVENT_DICT_TYPE] = None,
     verbose: bool = False,
 ) -> None:
+    async def handle_message(msg: str) -> None:
+
+        try:
+            data = json.loads(msg)
+        except json.decoder.JSONDecodeError as e:
+            logger.error(f"Invalid data: '{msg}'.")
+            logger.debug(e)
+            return
+
+        if not isinstance(data, dict):
+            logger.error(f"Invalid data: '{data}'.")
+            return
+
+        if "request" in data:  # ...then it is RPC
+
+            req_type = data["request"]
+
+            try:
+                rpc_cls, rpc_cb = rpc_dict[req_type]
+            except KeyError:
+                logger.error(f"Unknown RPC request: {data}.")
+                return
+
+            assert req_type == rpc_cls.__name__
+
+            try:
+                req = rpc_cls.Request.from_dict(data)
+            except ValidationError as e:
+                logger.error(f"Invalid RPC: {data}, error: {e}")
+                return
+            except Arcor2Exception as e:
+                # this might happen if e.g. some dataclass does additional validation of values in its __post_init__
+                try:
+                    await client.send(rpc_cls.Response(data["id"], False, messages=[str(e)]).to_json())
+                    logger.debug(e, exc_info=True)
+                except KeyError:
+                    pass
+                return
+
+            else:
+
+                try:
+                    rpc_start = time.monotonic()
+                    resp = await rpc_cb(req, client)
+                    rpc_dur = time.monotonic() - rpc_start
+                    if rpc_dur > MAX_RPC_DURATION:
+                        logger.warn(f"{req.request} callback took {rpc_dur:.3f}s.")
+
+                except Arcor2Exception as e:
+                    logger.debug(e, exc_info=True)
+                    resp = rpc_cls.Response(req.id, False, [str(e)])
+                else:
+                    if resp is None:  # default response
+                        resp = rpc_cls.Response(req.id, True)
+                    else:
+                        assert isinstance(resp, rpc_cls.Response)
+                        resp.id = req.id
+
+            await client.send(resp.to_json())
+
+            if logger.level == LogLevel.DEBUG:
+
+                # Silencing of repetitive log messages
+                # ...maybe this could be done better and in a more general way using logging.Filter?
+
+                now = time.monotonic()
+                if req.request not in req_last_ts:
+                    req_last_ts[req.request] = deque()
+
+                while req_last_ts[req.request]:
+                    if req_last_ts[req.request][0] < now - 5.0:
+                        req_last_ts[req.request].popleft()
+                    else:
+                        break
+
+                req_last_ts[req.request].append(now)
+                req_per_sec = len(req_last_ts[req.request]) / 5.0
+
+                if req_per_sec > 2:
+                    if req.request not in ignored_reqs:
+                        ignored_reqs.add(req.request)
+                        logger.debug(f"Request of type {req.request} will be silenced.")
+                elif req_per_sec < 1:
+                    if req.request in ignored_reqs:
+                        ignored_reqs.remove(req.request)
+
+                if req.request not in ignored_reqs:
+                    # TODO do not print out too big messages (ideally omit its data part)
+                    logger.debug(f"RPC request: {req}, result: {resp}")
+
+        elif "event" in data:  # ...event from UI
+
+            assert event_dict
+
+            try:
+                event_cls, event_cb = event_dict[data["event"]]
+            except KeyError as e:
+                logger.error(f"Unknown event type: {e}.")
+                return
+
+            try:
+                event = event_cls.from_dict(data)
+            except ValidationError as e:
+                logger.error(f"Invalid event: {data}, error: {e}")
+                return
+
+            await event_cb(event, client)
+
+        else:
+            logger.error(f"unsupported format of message: {data}")
 
     if event_dict is None:
         event_dict = {}
@@ -57,114 +167,11 @@ async def server(
 
         await register(client)
 
+        loop = asyncio.get_event_loop()
+
         async for message in client:
+            loop.create_task(handle_message(message))
 
-            try:
-                data = json.loads(message)
-            except json.decoder.JSONDecodeError as e:
-                logger.error(f"Invalid data: '{message}'.")
-                logger.debug(e)
-                continue
-
-            if not isinstance(data, dict):
-                logger.error(f"Invalid data: '{data}'.")
-                continue
-
-            if "request" in data:  # ...then it is RPC
-
-                req_type = data["request"]
-
-                try:
-                    rpc_cls, rpc_cb = rpc_dict[req_type]
-                except KeyError:
-                    logger.error(f"Unknown RPC request: {data}.")
-                    continue
-
-                assert req_type == rpc_cls.__name__
-
-                try:
-                    req = rpc_cls.Request.from_dict(data)
-                except ValidationError as e:
-                    logger.error(f"Invalid RPC: {data}, error: {e}")
-                    continue
-                except Arcor2Exception as e:
-                    # this might happen if e.g. some dataclass does additional validation of values in its __post_init__
-                    try:
-                        await client.send(rpc_cls.Response(data["id"], False, messages=[str(e)]).to_json())
-                        logger.debug(e, exc_info=True)
-                    except KeyError:
-                        pass
-                    continue
-
-                else:
-
-                    try:
-                        rpc_start = time.monotonic()
-                        resp = await rpc_cb(req, client)
-                        rpc_dur = time.monotonic() - rpc_start
-                        if rpc_dur > MAX_RPC_DURATION:
-                            logger.warn(f"{req.request} callback took {rpc_dur:.3f}s.")
-
-                    except Arcor2Exception as e:
-                        logger.debug(e, exc_info=True)
-                        resp = rpc_cls.Response(req.id, False, [str(e)])
-                    else:
-                        if resp is None:  # default response
-                            resp = rpc_cls.Response(req.id, True)
-                        else:
-                            assert isinstance(resp, rpc_cls.Response)
-                            resp.id = req.id
-
-                await client.send(resp.to_json())
-
-                if logger.level == LogLevel.DEBUG:
-
-                    # Silencing of repetitive log messages
-                    # ...maybe this could be done better and in a more general way using logging.Filter?
-
-                    now = time.monotonic()
-                    if req.request not in req_last_ts:
-                        req_last_ts[req.request] = deque()
-
-                    while req_last_ts[req.request]:
-                        if req_last_ts[req.request][0] < now - 5.0:
-                            req_last_ts[req.request].popleft()
-                        else:
-                            break
-
-                    req_last_ts[req.request].append(now)
-                    req_per_sec = len(req_last_ts[req.request]) / 5.0
-
-                    if req_per_sec > 2:
-                        if req.request not in ignored_reqs:
-                            ignored_reqs.add(req.request)
-                            logger.debug(f"Request of type {req.request} will be silenced.")
-                    elif req_per_sec < 1:
-                        if req.request in ignored_reqs:
-                            ignored_reqs.remove(req.request)
-
-                    if req.request not in ignored_reqs:
-                        # TODO do not print out too big messages (ideally omit its data part)
-                        asyncio.ensure_future(logger.debug(f"RPC request: {req}, result: {resp}"))
-
-            elif "event" in data:  # ...event from UI
-
-                try:
-                    event_cls, event_cb = event_dict[data["event"]]
-                except KeyError as e:
-                    logger.error(f"Unknown event type: {e}.")
-                    continue
-
-                try:
-                    event = event_cls.from_dict(data)
-                except ValidationError as e:
-                    logger.error(f"Invalid event: {data}, error: {e}")
-                    continue
-
-                await event_cb(event, client)
-
-            else:
-                logger.error(f"unsupported format of message: {data}")
     except websockets.exceptions.ConnectionClosed:
         pass
     finally:
