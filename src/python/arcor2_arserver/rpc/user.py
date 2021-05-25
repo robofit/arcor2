f031739d28fb132d601a675736994de6ff6abe8d
ZdenekM <imaterna@fit.vutbr.cz>
2021-05-25 14:45:06 +0200

    feat(arcor2_arserver): allow fast re-login after connection was broken
    - When the device e.g. lost connection, it was impossible to log in with the same user name for 20s.
    - Server now tries if the stored connection is ok and if not, it allows re-use of the user name.

--- src/python/arcor2_arserver/rpc/user.py
+++ src/python/arcor2_arserver/rpc/user.py
@@ -7,7 +7,7 @@ from arcor2_arserver_data import rpc as srpc
 
 async def register_user_cb(req: srpc.u.RegisterUser.Request, ui: WsClient) -> None:
 
-    glob.USERS.login(req.args.user_name, ui)
+    await glob.USERS.login(req.args.user_name, ui)
     glob.logger.debug(f"User {req.args.user_name} just logged in. Known user names are: {glob.USERS.user_names}")
 
     await glob.LOCK.cancel_auto_release(req.args.user_name)
