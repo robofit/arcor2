907cf16b4352ba836a0b0a8b7c8c2d0d32032845
ZdenekM <imaterna@fit.vutbr.cz>
2021-06-29 09:27:43 +0200

    feat(arcor2): dummy multiarm robot

--- src/python/arcor2/test_objects/upload.py
+++ src/python/arcor2/test_objects/upload.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 from arcor2.data.object_type import Box as BoxModel
 from arcor2.object_types.upload import upload_def
 from arcor2.test_objects.box import Box
 from arcor2.test_objects.box2 import Box2
+from arcor2.test_objects.dummy_multiarm_robot import DummyMultiArmRobot
 from arcor2.test_objects.param_to_return import ParamToReturn
 from arcor2.test_objects.tester import Tester
 
@@ -15,6 +15,7 @@ def main() -> None:
     upload_def(Box2, BoxModel("Box2", 0.2, 0.2, 0.2))
     upload_def(Tester, BoxModel("Tester", 0.3, 0.3, 0.3))
     upload_def(ParamToReturn)
+    upload_def(DummyMultiArmRobot)
 
 
 if __name__ == "__main__":
