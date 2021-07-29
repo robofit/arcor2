366b8e5d99ee5f55a8491f239fbc594a43bdb768
ZdenekM <imaterna@fit.vutbr.cz>
2021-07-29 08:44:21 +0200

    fix(arcor2_arserver): aiming db prunning

--- src/python/arcor2_arserver/rpc/user.py
+++ src/python/arcor2_arserver/rpc/user.py
@@ -1,7 +1,10 @@
+import asyncio
+
 from websockets.server import WebSocketServerProtocol as WsClient
 
 from arcor2_arserver import globals as glob
 from arcor2_arserver.lock.structures import LockEventData
+from arcor2_arserver.rpc.objects import object_aiming_prune
 from arcor2_arserver_data import rpc as srpc
 
 
@@ -12,12 +15,15 @@ async def register_user_cb(req: srpc.u.RegisterUser.Request, ui: WsClient) -> No
 
     await glob.LOCK.cancel_auto_release(req.args.user_name)
 
+    # those are locks that are known for all users
     for user, user_objects in glob.LOCK.all_ui_locks.items():
         if user != req.args.user_name:
             glob.LOCK.notifications_q.put_nowait(LockEventData(user_objects, user, True, ui))
 
-    _, user_write_locks = await glob.LOCK.get_owner_locks(req.args.user_name)
-    if user_write_locks:
+    # these locks are known only to the current user
+    if user_write_locks := (await glob.LOCK.get_owner_locks(req.args.user_name))[1]:
         glob.LOCK.notifications_q.put_nowait(LockEventData(user_write_locks, req.args.user_name, True, ui))
+
+    asyncio.create_task(object_aiming_prune())
 
     return None
