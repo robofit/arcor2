4f0c2074905423325b8af4e7d016e2aee2f74050
ZdenekM <imaterna@fit.vutbr.cz>
2021-09-15 15:34:06 +0200

    fix: it was not possible to go online with VCO

--- src/python/arcor2_arserver/tests/test_objects.py
+++ src/python/arcor2_arserver/tests/test_objects.py
@@ -171,6 +171,22 @@ def test_virtual_collision_object(start_processes: None, ars: ARServer) -> None:
     assert scn_evt.change_type == ot_evt.Type.ADD
     assert scn_evt.data.name == name
 
+    assert ars.call_rpc(
+        rpc.s.StartScene.Request(get_id()),
+        rpc.s.StartScene.Response,
+    ).result
+
+    assert event(ars, events.s.SceneState).data.state == events.s.SceneState.Data.StateEnum.Starting
+    assert event(ars, events.s.SceneState).data.state == events.s.SceneState.Data.StateEnum.Started
+
+    assert ars.call_rpc(
+        rpc.s.StopScene.Request(get_id()),
+        rpc.s.StopScene.Response,
+    ).result
+
+    assert event(ars, events.s.SceneState).data.state == events.s.SceneState.Data.StateEnum.Stopping
+    assert event(ars, events.s.SceneState).data.state == events.s.SceneState.Data.StateEnum.Stopped
+
     delete_res_2 = ars.call_rpc(
         rpc.s.RemoveFromScene.Request(get_id(), rpc.s.RemoveFromScene.Request.Args(scn_evt.data.id)),
         rpc.s.RemoveFromScene.Response,
@@ -183,7 +199,7 @@ def test_virtual_collision_object(start_processes: None, ars: ARServer) -> None:
     assert scn_evt2.data.name == name
 
     ot_evt2 = event(ars, events.o.ChangedObjectTypes)
-    assert ot_evt2.change_type == ot_evt.Type.REMOVE
+    assert ot_evt2.change_type == ot_evt2.Type.REMOVE
     assert len(ot_evt2.data) == 1
     assert ot_evt2.data[0].object_model == om
     assert ot_evt2.data[0].type == name
