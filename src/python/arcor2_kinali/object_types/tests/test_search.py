bff0314e5783888da97143efc6f36e4232670922
ZdenekM <imaterna@fit.vutbr.cz>
2021-10-07 10:40:40 +0200

    chore(arcor2_kinali): package removed
    - Kinali will develop those on their own.

--- src/python/arcor2_kinali/object_types/tests/test_search.py
+++
@@ -1,7 +0,0 @@
-from arcor2.object_types.utils import check_object_type
-from arcor2_kinali.object_types.search import Search
-
-
-def test_object_type() -> None:
-    check_object_type(Search)
-    assert not Search.abstract()
