6694f41b47e29c416d9edda26de2fe76d0605600
ZdenekM <imaterna@fit.vutbr.cz>
2022-02-02 10:20:47 +0100

    build(kinect): use bundled pyk4a
    - No need to install kinect sdk.
    - Docker image is now based on slim buster as other images.

--- build-support/install_kinect_prerequisites.sh
+++
@@ -1,9 +0,0 @@
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-add-repository https://packages.microsoft.com/ubuntu/"$(lsb_release -rs)"/prod
-sudo apt-get update
-export DEBIAN_FRONTEND="noninteractive"
-echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
-echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | sudo debconf-set-selections
-sudo apt-get install -y libk4a1.4 libk4a1.4-dev
-sudo wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules -P /etc/udev/rules.d/
-sudo udevadm control --reload-rules && udevadm trigger
