bafdf0344106ab3f75eebe3d4ade852628040efd
cherniaev <chrnyaevek@gmail.com>
2022-07-01 14:35:51 +0200

    feat(arcor2_dobot)!: update service, implement new error handling flow

---
+++ src/python/arcor2_dobot/exceptions.py
@@ -0,0 +1,21 @@
+from arcor2.flask import FlaskException, General, WebApiErrorFactory
+from arcor2_dobot import __name__ as package_name
+
+
+class DobotException(FlaskException):
+    service = package_name
+
+
+class DobotGeneral(FlaskException):
+    description = General.description
+
+
+class NotFound(DobotException):
+    description = "Occurs when something is not found"
+
+
+class StartError(DobotException):
+    description = "Occurs when start condition is not met"
+
+
+WebApiError = WebApiErrorFactory.get_class(DobotGeneral, NotFound, StartError)
