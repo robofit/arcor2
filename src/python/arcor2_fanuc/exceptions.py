315f87ad725bc6dc33350b5427723b8e50875f59
cherniaev <chrnyaevek@gmail.com>
2022-07-01 14:55:48 +0200

    feat(arcor2_fanuc)!: update service, implement new error handling flow

---
+++ src/python/arcor2_fanuc/exceptions.py
@@ -0,0 +1,21 @@
+from arcor2.flask import FlaskException, General, WebApiErrorFactory
+from arcor2_fanuc import __name__ as package_name
+
+
+class FanucException(FlaskException):
+    service = package_name
+
+
+class FanucGeneral(FlaskException):
+    description = General.description
+
+
+class NotFound(FanucException):
+    description = "Occurs when something is not found."
+
+
+class StartError(FanucException):
+    description = "Occurs when start condition is not met."
+
+
+WebApiError = WebApiErrorFactory.get_class(FanucGeneral, StartError, NotFound)
