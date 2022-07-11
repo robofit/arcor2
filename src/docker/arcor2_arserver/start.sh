33549d18cbf1aa05776b8fee8ed09e926c46cf50
ZdenekM <imaterna@fit.vutbr.cz>
2022-07-11 10:23:16 +0200

    chore: added support for shell scripts

--- src/docker/arcor2_arserver/start.sh
+++ src/docker/arcor2_arserver/start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash  
+#!/bin/bash
 
-mkdir -p ${ARCOR2_AREDITOR_LOGS_FOLDER:-/root/logs}
-python3 -m uploadserver --directory ${ARCOR2_AREDITOR_LOGS_FOLDER:-/root/logs} 6799 &
+mkdir -p "${ARCOR2_AREDITOR_LOGS_FOLDER:-/root/logs}"
+python3 -m uploadserver --directory "${ARCOR2_AREDITOR_LOGS_FOLDER:-/root/logs}" 6799 &
 cd /root || exit
 ./arserver.pex
