5c2a41894ca26a10da5c65485dc7df868381f9c4
ZdenekM <imaterna@fit.vut.cz>
2024-10-01 10:21:12 +0200

    feat(arcor2_ur): calibration file

--- src/docker/arcor2_ur/start.sh
+++ src/docker/arcor2_ur/start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+set -e
+
 # shellcheck source=/dev/null
 source /opt/ros/jazzy/setup.bash
 
 : "${UR_TYPE:=ur5e}"
-: "${ROBOT_CONTAINER_NAME:=ur-demo-ursim}"
 
-ROBOT_CONTAINER_IP=$(getent hosts "$ROBOT_CONTAINER_NAME" | awk '{ print $1 }')
+# simulator needs some time to get running...
+if [[ -n "$SLEEP" && "$SLEEP" =~ ^[0-9]+$ ]]; then
+	echo "Waiting for $SLEEP seconds..."
+	sleep "$SLEEP"
+fi
 
-sleep 10s # TODO find a way how to detect that the robot is up and running (test if some port is opened?)
+cp --update=none "$(ros2 pkg prefix --share ur_description)/config/$UR_TYPE/default_kinematics.yaml" /root/robot_calibration.yaml
 
-ros2 launch ur_robot_driver ur_control.launch.py ur_type:="$UR_TYPE" robot_ip:="$ROBOT_CONTAINER_IP" launch_rviz:=false &
+ros2 launch ur_robot_driver ur_control.launch.py ur_type:="$UR_TYPE" robot_ip:="$ROBOT_IP" launch_rviz:=false kinematics_params_file:="/root/robot_calibration.yaml" &
 
 PEX_EXTRA_SYS_PATH=/opt/ros/jazzy/lib/python3.12/site-packages PYTHONOPTIMIZE=1 /bin/app/pex
--- src/python/arcor2_ur/CHANGELOG.md
+++ src/python/arcor2_ur/CHANGELOG.md
@@ -2,6 +2,13 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
+## [1.3.0] - 2024-10-01
+
+### Changed
+
+- Docker image now checks for a robot calibration file at `/root/robot_calibration.yaml`. If not given, a default one is used.
+- `ROBOT_CONTAINER_IP` was changed to `ROBOT_IP`.
+
 ## [1.2.1] - 2024-09-30
 
 ### Fixed
