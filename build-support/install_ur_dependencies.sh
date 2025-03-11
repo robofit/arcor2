600e13390cc84d7833aeecc1681e74010be07115
ZdenekM <imaterna@fit.vut.cz>
2025-03-11 10:00:06 +0100

    chore(compose): added magician to second instance of arserver

--- build-support/install_ur_dependencies.sh
+++ build-support/install_ur_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get install -y -q --no-install-recommends \
-	ros-jazzy-ros-base=0.11.0-1noble.20250115.204954 \
-	ros-jazzy-ur=3.0.2-1noble.20250202.035908 \
-	ros-jazzy-moveit-py=2.12.1-1noble.20250202.033533
+	ros-jazzy-ros-base=0.11.0-1noble.20250210.212512 \
+	ros-jazzy-ur=3.1.0-1noble.20250305.200835 \
+	ros-jazzy-moveit-py=2.12.2-1noble.20250218.181017
