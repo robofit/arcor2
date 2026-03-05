c34760ebbb23b3e8f9e1408241f09d2a7ac6fa5a
Simon Kadnar <simonkadnar@gmail.com>
2026-03-05 22:36:51 +0100

    chore(arcor2_ur): update pinned ROS Jazzy dependency versions

--- build-support/install_ur_dependencies.sh
+++ build-support/install_ur_dependencies.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-apt-get update
-
-# debug - ukáž dostupnosť a verzie presne tých balíkov čo inštaluješ
-apt-cache policy ros-jazzy-ros-base ros-jazzy-ur ros-jazzy-moveit-py || true
-apt-cache madison ros-jazzy-ros-base ros-jazzy-ur ros-jazzy-moveit-py || true
-
 apt-get install -y -q --no-install-recommends \
-  ros-jazzy-ros-base=0.11.0-1noble.20251108.003726 \
-  ros-jazzy-ur=3.6.0-1noble.20251114.095610 \
-  ros-jazzy-moveit-py=2.12.3-1noble.20251108.011222
+  ros-jazzy-ros-base=0.11.0-1noble.20260126.203129 \
+  ros-jazzy-ur=3.7.0-1noble.20260126.222208 \
+  ros-jazzy-moveit-py=2.12.4-1noble.20260126.215035
