88d7dc4cde0863c198ecc9ce1c4a53978dbff301
Simon Kadnar <simonkadnar@gmail.com>
2026-03-05 22:32:11 +0100

    fix(arcor_ur): debug build

--- build-support/install_ur_dependencies.sh
+++ build-support/install_ur_dependencies.sh
@@ -3,10 +3,11 @@ set -euxo pipefail
 
 apt-get update
 
-# debug check
-apt-cache search ros-jazzy | head || true
+# debug - ukáž dostupnosť a verzie presne tých balíkov čo inštaluješ
+apt-cache policy ros-jazzy-ros-base ros-jazzy-ur ros-jazzy-moveit-py || true
+apt-cache madison ros-jazzy-ros-base ros-jazzy-ur ros-jazzy-moveit-py || true
 
 apt-get install -y -q --no-install-recommends \
-    ros-jazzy-ros-base=0.11.0-1noble.20251108.003726 \
-    ros-jazzy-ur=3.6.0-1noble.20251114.095610 \
-    ros-jazzy-moveit-py=2.12.3-1noble.20251108.011222
+  ros-jazzy-ros-base=0.11.0-1noble.20251108.003726 \
+  ros-jazzy-ur=3.6.0-1noble.20251114.095610 \
+  ros-jazzy-moveit-py=2.12.3-1noble.20251108.011222
